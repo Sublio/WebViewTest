2587cd5e87530dd6d59bf32d4d03e7f3c842028b
Sublio <sublio1260@gmail.com>
2017-06-09 11:56:49 +0600

    Multiple destinations

--- testOnDemand.sh
+++ testOnDemand.sh
@@ -4,4 +4,6 @@ xcodebuild \
   -scheme WebViewTest\
   -sdk iphonesimulator \
   -destination 'platform=iOS Simulator,name=iPhone 5,OS=8.4' \
+  -destination 'platform=iOS Simulator,name=iPhone 5,OS=9.3' \
+   -destination 'platform=iOS Simulator,name=iPhone 5,OS=10.3.1' \
   test | xcpretty
